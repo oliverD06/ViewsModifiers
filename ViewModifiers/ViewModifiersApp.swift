b9e1269c426ad88a78647ec43919727a47c4b5af
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-09-15 10:38:36 -0500

    Initial Commit

---
+++ ViewModifiers/ViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifiersApp.swift
+//  ViewModifiers
+//
+//  Created by Oliver Delaney on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
